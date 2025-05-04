110d3879a5dd278ecd181fb1de97e190baf484f5
Corey Caskey <coreyrcaskey@gmail.com>
2025-05-04 15:47:03 -0400

    fix

--- v7/vite.config.ts
+++ v7/vite.config.ts
@@ -1,9 +1,9 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { reactRouter } from "@react-router/dev/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [reactRouter(), tsconfigPaths()],
 });
